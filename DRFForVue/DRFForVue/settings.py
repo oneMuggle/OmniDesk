2e18e283fe39acd02108284c041250491cb0ec08
麻瓜 一只 <2397801926@qq.com>
2025-04-11 17:19:31 +0800

    fix(settings): 调整JWT令牌生命周期

--- DRFForVue/DRFForVue/settings.py
+++ DRFForVue/DRFForVue/settings.py
@@ -206,8 +206,8 @@ REST_FRAMEWORK = {
 
 # JWT配置
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=120),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'AUTH_HEADER_TYPES': ('Bearer',),
