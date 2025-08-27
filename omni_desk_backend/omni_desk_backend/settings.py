41eed6ca5450d9bbabe52e0caf77eb3728c06c61
麻瓜 一只 <2397801926@qq.com>
2025-08-27 22:08:23 +0800

    ops: 更新Docker配置和静态文件服务设置

--- omni_desk_backend/omni_desk_backend/settings.py
+++ omni_desk_backend/omni_desk_backend/settings.py
@@ -194,14 +194,11 @@ LOGGING = {
 }
 
 # Static files
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_URL = '/django-static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
