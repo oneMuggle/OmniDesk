9666c81758451a6af91f6dd20c6d8cf46ca72342
onemuggle <2397801926@qq.com>
2026-08-09 23:53:45 +0800

    style: ruff format memos/serializers.py(修复 lint-backend)

--- omni_desk_backend/memos/serializers.py
+++ omni_desk_backend/memos/serializers.py
@@ -13,11 +13,7 @@ class MemoSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
         # P0-2:改期到未来时重置提醒标记,使新时间点可再次触发到期提醒
         new_reminder_time = validated_data.get("reminder_time")
-        if (
-            new_reminder_time is not None
-            and new_reminder_time > timezone.now()
-            and instance.reminder_sent
-        ):
+        if new_reminder_time is not None and new_reminder_time > timezone.now() and instance.reminder_sent:
             validated_data["reminder_sent"] = False
         # 使用父类的 update 方法来处理所有字段的更新
         # 这是最健壮和推荐的做法
